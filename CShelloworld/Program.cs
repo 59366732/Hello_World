8473dd4b60a6f9929206d0354b2a53fdaa4380b7
Ing Saeyang <ings59@email.nu.ac.th>
2020-06-12 23:48:28 +0700

    restore all

---
+++ CShelloworld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CShelloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello csharp World!");
+            Console.WriteLine("This is the first program test!");
+        }
+    }
+}
