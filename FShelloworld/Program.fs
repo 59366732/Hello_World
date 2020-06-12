2bf7a2f4d24e32ab5c52f53a05e16223f42dc7c8
Ing Saeyang <ings59@email.nu.ac.th>
2020-06-12 23:47:08 +0700

    remove all except qs

--- FShelloworld/Program.fs
+++
@@ -1,9 +0,0 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    printfn "This is the first program test!"
-    0 // return an integer exit code
--- VBhelloworld/Program.vb
+++
@@ -1,8 +0,0 @@
-Imports System
-
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("This is the first program test!")
-    End Sub
-End Module
